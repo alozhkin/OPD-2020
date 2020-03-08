4510af93efb2949a29f0fb84ceb368e9e0ec400b
Kirill <7kreker7@gmail.com>
2020-03-08 15:19:21 +0300

    ver 0.4

--- src/test/java/OPD/WebCrawler/TestCrawler.java
+++ src/test/java/OPD/WebCrawler/TestCrawler.java
@@ -7,12 +7,18 @@ import org.jsoup.nodes.Document;
 import java.io.IOException;
 
 public class TestCrawler {
-    public static String rootURL = "https://lms.spbstu.ru/login/index.php";
+    public static String rootURL = "https://jsoup.org/";
+    public static String rootURL2 = "https://jsoup.org/download";
 
     public static void main(@NotNull String[] args) {
         try {
             Document document = Jsoup.connect(rootURL).get();
             Links list = new Links();
+            list.addAll(WebCrawler.crawl(document, rootURL, list).get());
+            list.printALL();
+            System.out.println("*********************Переход по ссылке****************************");
+
+            document = Jsoup.connect(rootURL2).get();
             list.addAll(WebCrawler.crawl(document, rootURL, list).get());
             list.printALL();
         } catch (IOException e) {
