12fac174be9837ddb681aee8ed87a4486b3225ab
Kvarki-rM/develop <7kreker7@gmail.com>
2020-03-22 14:25:58 +0300

    ????

--- src/main/java/crawler/DefaultLinkFilter.java
+++ src/main/java/crawler/DefaultLinkFilter.java
@@ -12,17 +12,17 @@ import java.util.List;
 import java.util.Set;
 
 public class DefaultLinkFilter implements LinkFilter {
-    List<String> fileDate;
+   // List<String> fileDate;
     //List<String> langDate;
 
-    {
-        try {
-          //langDate = new ArrayList<>(Files.readAllLines(Paths.get("src/main/resources/language.txt")));
-            fileDate = new ArrayList<>(Files.readAllLines(Paths.get("src/main/resources/FilesTeg.txt")));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+   // {
+   //     try {
+   //       //langDate = new ArrayList<>(Files.readAllLines(Paths.get("src/main/resources/language.txt")));
+   //         fileDate = new ArrayList<>(Files.readAllLines(Paths.get("src/main/resources/FilesTeg.txt")));
+   //     } catch (IOException e) {
+   //         e.printStackTrace();
+   //     }
+   // }
 
     public Set<Link> filter(@NotNull List<Link> links, String domain) {
 
