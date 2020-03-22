44ffa5602b282b0c434dab51422bd6c032b0a104
Kvarki-rM/develop <7kreker7@gmail.com>
2020-03-22 14:28:27 +0300

    ????

--- src/main/java/crawler/DefaultLinkFilter.java
+++ src/main/java/crawler/DefaultLinkFilter.java
@@ -3,10 +3,6 @@ package crawler;
 import org.jetbrains.annotations.NotNull;
 import utils.Link;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
