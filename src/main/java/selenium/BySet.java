f04c29ef5f8472e945512ae202b430c021b27edd
ВашеEvgenyKuzingit config --global user.email Ваш < evgenykuzin21@gmail.com>
2020-03-23 19:53:03 +0300

    в BySet изменен тип аргумента на 'String...'. фикс WebDriverManager.

--- src/main/java/selenium/BySet.java
+++ src/main/java/selenium/BySet.java
@@ -1,65 +1,64 @@
 package selenium;
 
 import org.openqa.selenium.By;
-
 import java.util.HashSet;
 
 public class BySet extends HashSet<By> {
 
     public class Builder {
 
-        public Builder addTagNames(Object... selectors){
-            for (Object o : selectors) {
-                add(By.tagName((String) o));
+        public Builder addTagNames(String... selectors){
+            for (String o : selectors) {
+                add(By.tagName(o));
             }
             return this;
         }
 
-        public Builder addClassNames(Object... selectors) {
-            for (Object o : selectors) {
-                add(By.className((String) o));
+        public Builder addClassNames(String... selectors) {
+            for (String o : selectors) {
+                add(By.className(o));
             }
             return this;
         }
 
-        public Builder addNames(Object... selectors) {
-            for (Object o : selectors) {
-                add(By.name((String) o));
+        public Builder addNames(String... selectors) {
+            for (String o : selectors) {
+                add(By.name(o));
             }
             return this;
         }
 
-        public Builder addIds(Object... selectors) {
-            for (Object o : selectors) {
-                add(By.id((String) o));
+        public Builder addIds(String... selectors) {
+            for (String o : selectors) {
+                add(By.id(o));
             }
             return this;
         }
 
-        public Builder addXpaths(Object... selectors) {
-            for (Object o : selectors) {
-                add(By.xpath((String) o));
+        public Builder addXpaths(String... selectors) {
+            for (String o : selectors) {
+                add(By.xpath(o));
             }
             return this;
         }
 
-        public Builder addLinkTexts(Object... selectors) {
-            for (Object o : selectors) {
-                add(By.linkText((String) o));
+        public Builder addLinkTexts(String... selectors) {
+            for (String o : selectors) {
+                add(By.linkText(o));
             }
             return this;
         }
 
-        public Builder addPartialLinkTexts(Object... selectors) {
-            for (Object o : selectors) {
-                add(By.partialLinkText((String) o));
+        public Builder addPartialLinkTexts(String... selectors) {
+            for (String o : selectors) {
+                add(By.partialLinkText(o));
             }
             return this;
         }
 
-        public Builder addCssSelectors(Object... selectors) {
-            for (Object o : selectors) {
-                add(By.cssSelector((String) o));
+        public Builder addCssSelectors(String... selectors) {
+            for (String o : selectors) {
+                add(By.cssSelector(o));
             }
             return this;
         }
