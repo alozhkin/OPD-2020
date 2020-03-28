b53287bdc9624b23892319173f2224be667372de
Kvarki-rM/develop <7kreker7@gmail.com>
2020-03-28 22:23:36 +0300

    Создан CrawlerTest

--- src/test/java/CrawlerTest.java
+++ src/test/java/CrawlerTest.java
@@ -1,21 +1,31 @@
-import crawler.Crawler;
 import crawler.DefaultCrawler;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import utils.Html;
+import utils.Link;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CrawlerTest {
-    Crawler crawler;
-
-    @BeforeEach
-    void init() {
-        crawler = new DefaultCrawler();
-    }
-
     @Test
-    void shouldNotFailOnEmptyHtml() {
-        assertEquals(0, crawler.crawl(Html.emptyHtml()).size());
+    public void drillingUrlOutput() throws IOException {
+        Html htmlSite = Html.fromFile(Path.of("src/test/resourcestelefort.spb.ru.html"));
+        Set<String> InList = Set.of(
+                "http://telefort.spb.ru/contacts.htm",
+                "http://telefort.spb.ru/obj.pdf",
+                "http://metrika.yandex.ru/stat/?id=6380740&from=informer",
+                "https://pbx.telefort.spb.ru/",
+                "http://telefort.spb.ru/partners.htm",
+                "http://telefort.spb.ru/index.html",
+                "http://telefort.spb.ru/vacancy.htm",
+                "http://telefort.spb.ru/documents.htm"
+        );
+        Set<Link> editedInList = InList.stream().map(Link::new).collect(Collectors.toSet());
+        assertEquals(editedInList,
+                new DefaultCrawler().crawl(new Html(htmlSite.toString(), new Link("http://telefort.spb.ru/"))));
     }
 }
