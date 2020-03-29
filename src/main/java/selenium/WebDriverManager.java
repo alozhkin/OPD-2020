6be1eab8c7476b400310550a853de78800045435
ВашеEvgenyKuzingit config --global user.email Ваш < evgenykuzin21@gmail.com>
2020-03-29 18:40:13 +0300

    refactoring. new methods (you can click or not click to get dynamic content). problem: connecting to some sites is too slow.

--- src/main/java/selenium/WebDriverManager.java
+++ src/main/java/selenium/WebDriverManager.java
@@ -1,19 +1,27 @@
 package selenium;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import diff_match_patch.DiffMatchPatch;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import utils.Html;
 import utils.Link;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class WebDriverManager {
     private Link currentLink;
     private WebDriver driver;
     private Html currentHtml;
+    private static final DiffMatchPatch diffMatchPatch = new DiffMatchPatch();
+    private static Set<WebDriver> drivers = ConcurrentHashMap.newKeySet();
 
     public WebDriverManager() {
         this(null);
@@ -24,10 +32,19 @@ public class WebDriverManager {
     }
 
     public void init(Link link) {
-        driver = new ChromeDriver();
+        driver = initDriver();
         if (link != null) {
             connect(link);
         }
+    }
+
+    private static WebDriver initDriver() {
+        var options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.setPageLoadStrategy(PageLoadStrategy.EAGER);
+        var driver = new ChromeDriver(options);
+        drivers.add(driver);
+        return driver;
     }
 
     public void connect(Link link) {
@@ -37,7 +54,7 @@ public class WebDriverManager {
     }
 
     public Html parseHtml() {
-        return new Html(driver.getPageSource(), currentLink);
+        return new Html(driver.getPageSource(), new Link(driver.getCurrentUrl()));
     }
 
     public void setCurrentLink(Link link) {
@@ -56,25 +73,67 @@ public class WebDriverManager {
         return currentHtml;
     }
 
-    public Collection<Html> getDynamicContent(BySet bySet) {
-        Collection<WebElement> elements = new ArrayList<>();
-        for (By by : bySet) {
-            elements.addAll(driver.findElements(by));
-        }
-        ArrayList<Html> htmls = new ArrayList<>();
-        for (WebElement element : elements) {
-            element.click();
-            Html difference = getDifference(currentHtml, parseHtml());
-            if (difference != null) {
-                htmls.add(difference);
+    public Collection<String> getDynamicContent() {
+        Html html1 = parseHtmlWithJsoup();
+        Html html2 = parseHtml();
+        return getNewWords(html1, html2);
+    }
+
+    public Collection<String> getDynamicContentWithClicking(BySet bySet) {
+        Collection<String> result = new ArrayList<>();
+        List<WebElement> elements = new ArrayList<>();
+        int i = 0;
+        int border = 1000; // Не окончательное решение. Скорее всего предел нужно определять исходя из страницы.
+        while (i < border) {
+            for (By by : bySet) {
+                elements.addAll(driver.findElements(by));
             }
+            if (elements.isEmpty()) {
+                connect(currentLink);
+                continue;
+            }
+            WebElement element = elements.get(i);
+            i++;
+            try {
+                element.click();
+                result.addAll(getNewWords(currentHtml, parseHtml()));
+            } catch (ElementNotInteractableException | StaleElementReferenceException ignored) {
+            }
+
         }
 
-        return htmls;
+
+        return result;
     }
 
-    private Html getDifference(Html html1, Html html2) {
-        return null;
+    public Html parseHtmlWithJsoup(Link link) {
+        Connection connection = Jsoup.connect(link.getAbsoluteURL());
+        Html parsedHtml = new Html("", new Link(""));
+        try {
+            parsedHtml = new Html(connection.get().toString(), link);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return parsedHtml;
+    }
+    public Html parseHtmlWithJsoup() {
+        return parseHtmlWithJsoup(currentLink);
     }
 
+    public Collection<String> getNewWords(Html html1, Html html2) {
+        var a = Jsoup.parse(html1.toString()).text();
+        var b = Jsoup.parse(html2.toString()).text();
+        return diffMatchPatch.getNewWords(" " + a + " ", " " + b + " ");
+    }
+
+    public BySet constructBySet() {
+        BySet bySet = new BySet();
+        //есть идея читать html и парсить все элементы которые там есть и создавать из этого bySet)
+        //TODO
+        return bySet;
+    }
+
+    public void quit() {
+        driver.quit();
+    }
 }
