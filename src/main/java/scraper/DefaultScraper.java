8c823fe4ed623a21f500225dc832f574ef20f4ee
andrey <alozhkin99@gmail.com>
2020-04-18 16:36:38 +0300

    add opportunity to choose: ignore html without lang attr or not

--- src/main/java/scraper/DefaultScraper.java
+++ src/main/java/scraper/DefaultScraper.java
@@ -60,6 +60,8 @@ public class DefaultScraper implements Scraper {
             for (String siteLang : siteLangs.split(",")) {
                 if (siteLang.contains(htmlLang) || htmlLang.contains(siteLang)) return true;
             }
+        } else {
+            return System.getProperty("ignore.html.without.lang").equals("false");
         }
         return false;
     }
