d18c6bf883e58767885eb424d71ce7a1d6a8e0cb
andrey <alozhkin99@gmail.com>
2020-04-18 14:03:49 +0300

    refactor timeout feature a bit

--- src/main/java/main/DomainTask.java
+++ src/main/java/main/DomainTask.java
@@ -6,52 +6,45 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.*;
-import java.util.concurrent.atomic.AtomicLong;
 
 public class DomainTask {
-    public static AtomicLong submittedTasksCount = new AtomicLong(0);
-    public static AtomicLong completedTaskCount = new AtomicLong(0);
     private final Context context;
     private final BlockingQueue<Link> linkQueue;
-    ExecutorCompletionService<Collection<String>> cs;
+    private final ExecutorCompletionService<Collection<String>> cs;
     private final Link domain;
 
-    public DomainTask(Context context,
-                      BlockingQueue<Link> linkQueue,
-                      ExecutorCompletionService<Collection<String>> cs,
-                      Link domain) {
+    DomainTask(Context context,
+               BlockingQueue<Link> linkQueue,
+               ExecutorCompletionService<Collection<String>> cs,
+               Link domain) {
         this.context = context;
         this.linkQueue = linkQueue;
         this.cs = cs;
         this.domain = domain;
     }
 
-    boolean findTo(Collection<String> allWords) throws ExecutionException {
+    void findTo(Collection<String> allWords) {
         Main.debugLog.info("Domain Task - start executing site " + domain);
-        Set<Future<Collection<String>>> set = new HashSet<>();
-        set.add(cs.submit(new SiteTask(context, domain, linkQueue)::run));
-        submittedTasksCount.incrementAndGet();
-        // order is important
+        Set<Future<Collection<String>>> futures = new HashSet<>();
+        futures.add(cs.submit(new SiteTask(context, domain, linkQueue)::run));
         try {
-            while (!Thread.currentThread().isInterrupted()
-                    && (completedTaskCount.get() - submittedTasksCount.get() != 0 || linkQueue.size() != 0)) {
+            while (!Thread.currentThread().isInterrupted() && futures.size() != 0) {
                 var link = linkQueue.poll(50, TimeUnit.MILLISECONDS);
                 if (link != null) {
-                    set.add(cs.submit(new SiteTask(context, link, linkQueue)::run));
-                    submittedTasksCount.incrementAndGet();
+                    futures.add(cs.submit(new SiteTask(context, link, linkQueue)::run));
                 }
                 var wordsFuture = cs.poll(50, TimeUnit.MILLISECONDS);
                 if (wordsFuture != null) {
-                    set.remove(wordsFuture);
-                    completedTaskCount.incrementAndGet();
+                    futures.remove(wordsFuture);
                     allWords.addAll(wordsFuture.get());
                 }
             }
+        } catch (ExecutionException e) {
+            Main.debugLog.error("Domain Task - Failed on site " + domain, e);
         } catch (InterruptedException ignored) {}
         Main.debugLog.info("Domain Task - stop executing site " + domain);
-        for (Future<Collection<String>> f : set) {
+        for (Future<Collection<String>> f : futures) {
             f.cancel(true);
         }
-        return false;
     }
 }
