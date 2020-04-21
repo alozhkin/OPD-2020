14dce131c34201a828f863de6b2491aaaf741b89
alozhkin <43116987+alozhkin@users.noreply.github.com>
2020-04-22 00:54:18 +0300

    solver encoding problem in WebDriverManagerTest.java src

--- src/test/java/selenium/WebDriverManagerTest.java
+++ src/test/java/selenium/WebDriverManagerTest.java
@@ -1,25 +1,51 @@
 package selenium;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
 import config.ConfigurationUtils;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import utils.Html;
 import utils.Link;
 
 import java.util.ArrayList;
 import java.util.Collection;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
 public class WebDriverManagerTest {
+    WireMockServer wireMockServer;
 
     @BeforeAll
-    static void init() {
+    static void configure() {
         ConfigurationUtils.configure();
+    }
+
+    @BeforeEach
+    void initMock() {
+        wireMockServer = new WireMockServer();
+        wireMockServer.start();
+        WireMock.stubFor(
+                get(
+                        urlEqualTo("/first_button"))
+                        .willReturn(
+                                aResponse().withBody("<div id=\"worked\">Worked</div>")));
+    }
+
+    @AfterEach
+    void quitMock() {
+        wireMockServer.shutdown();
+    }
+
+    String getSitePath(String siteName){
+        String projectPath = System.getProperty("project.path");
+        return "file://" + projectPath + "/src/test/resources/DriverTestRes/"+siteName;
     }
 
     @Test
     void testGetDynamicContentWithClicking1() {
-        String url = "";
+        String url = getSitePath("index1.html");
         Link link = new Link(url);
         WebDriverManager webDriverManager = new WebDriverManager(link);
         BySet bySet = new BySet();
@@ -31,7 +57,7 @@ public class WebDriverManagerTest {
 
     @Test
     void testGetDynamicContentWithClicking2() {
-        WebDriverManager webDriverManager = new WebDriverManager(new Link(""));
+        WebDriverManager webDriverManager = new WebDriverManager(new Link(getSitePath("index1.html")));
         Collection<String> dynamicContent = webDriverManager.getDynamicContentWithClicking();
         Collection<String> expected = new ArrayList<>();
         expected.add("");
@@ -42,7 +68,7 @@ public class WebDriverManagerTest {
     @Test
     void contentShouldBeDifferent() {
         WebDriverManager webDriverManager = new WebDriverManager();
-        Link link = new Link("");
+        Link link = new Link(getSitePath("index1.html"));
         Html parsedHtml = webDriverManager.parseHtmlWithJsoup(link);
         webDriverManager.connect(link);
         Html dynamicHtml = webDriverManager.getCurrentHtml();
@@ -54,7 +80,7 @@ public class WebDriverManagerTest {
         BySet expectedBySet = new BySet().addTagNames(
                 "a", "pre", "b", "code", "h1", "i", "h2", "h3", "h4",
                 "script", "div", "p", "ul", "abbr", "li", "ol", "span", "ul", "br");
-        Link link = new Link("https://jsoup.org");
+        Link link = new Link(getSitePath("index3.html"));
         WebDriverManager manager = new WebDriverManager(link);
         BySet actualBySet = manager.constructBySet();
         Assertions.assertEquals(expectedBySet, actualBySet);
