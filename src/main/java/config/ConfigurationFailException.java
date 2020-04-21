6885bb201afe6e74c06d7496895fdc79b0f5c8c5
andrey <alozhkin99@gmail.com>
2020-04-22 00:03:24 +0300

    get rid of unused methods

--- src/main/java/config/ConfigurationFailException.java
+++ src/main/java/config/ConfigurationFailException.java
@@ -1,9 +1,6 @@
 package config;
 
 public class ConfigurationFailException extends RuntimeException {
-    public ConfigurationFailException() {
-        super();
-    }
 
     public ConfigurationFailException(String message) {
         super(message);
@@ -11,13 +8,5 @@ public class ConfigurationFailException extends RuntimeException {
 
     public ConfigurationFailException(String message, Throwable cause) {
         super(message, cause);
-    }
-
-    public ConfigurationFailException(Throwable cause) {
-        super(cause);
-    }
-
-    protected ConfigurationFailException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
