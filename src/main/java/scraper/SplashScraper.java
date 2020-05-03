7fd5adcb9f49fd0f55a90f5dbb268e493896fb80
andrey <alozhkin99@gmail.com>
2020-05-03 22:04:08 +0300

    add http 503 and 504 support

--- src/main/java/scraper/SplashScraper.java
+++ src/main/java/scraper/SplashScraper.java
@@ -35,46 +35,6 @@ public class SplashScraper implements Scraper {
         makeRequestToHtmlRenderer(link, consumer);
     }
 
-    void makeRequestToHtmlRenderer(Link link, Consumer<Html> consumer) {
-        var request = rendererRequestFactory.getRequest(new DefaultSplashRequestContext.Builder(link).build());
-        var call = httpClient.newCall(request);
-        calls.add(call);
-        call.enqueue(new Callback() {
-            //todo fail support
-            @Override
-            public void onFailure(@NotNull Call call, @NotNull IOException e) {
-                System.out.println(call.request().url() + " " + e.toString());
-                calls.remove(call);
-            }
-
-            @Override
-            public void onResponse(@NotNull Call call, @NotNull Response response) {
-                handleResponse(response, call, link, consumer);
-                calls.remove(call);
-            }
-        });
-    }
-
-    void handleResponse(Response response, Call call, Link link, Consumer<Html> consumer) {
-        try (response) {
-            //todo 503, 504 support
-            if (response.code() != 200) {
-                throw new IOException();
-            }
-            //todo redirect support
-            var responseBody = response.body();
-            if (responseBody == null) {
-                throw new IOException("Response body is absent");
-            }
-            var html = new Html(responseBody.string(), link);
-            if (!call.isCanceled()) {
-                consumer.accept(html);
-            }
-        } catch (IOException e) {
-            LoggerUtils.debugLog.error("SplashScraper - Connection failed " + link, e);
-        }
-    }
-
     @Override
     public int scrapingSitesCount() {
         return httpClient.dispatcher().runningCallsCount();
@@ -88,5 +48,61 @@ public class SplashScraper implements Scraper {
     @Override
     public void shutdown() {
         httpClient.dispatcher().executorService().shutdown();
+    }
+
+    private void makeRequestToHtmlRenderer(Link link, Consumer<Html> consumer) {
+        var request = rendererRequestFactory.getRequest(new DefaultSplashRequestContext.Builder(link).build());
+        var call = httpClient.newCall(request);
+        calls.add(call);
+        call.enqueue(new SplashCallback(link, consumer));
+    }
+
+    //todo внутренняя ссылка не увеличивает размер объекта сильно?
+    private class SplashCallback implements Callback {
+        private final Link link;
+        private final Consumer<Html> consumer;
+
+        public SplashCallback(Link link, Consumer<Html> consumer) {
+            this.link = link;
+            this.consumer = consumer;
+        }
+
+        //todo fail support
+        @Override
+        public void onFailure(@NotNull Call call, @NotNull IOException e) {
+            LoggerUtils.consoleLog.error("SplashScraper - Request failed " + call.request().url().toString(), e);
+            calls.remove(call);
+        }
+
+        @Override
+        public void onResponse(@NotNull Call call, @NotNull Response response) {
+            handleResponse(response, call, link, consumer);
+            calls.remove(call);
+        }
+
+        private void handleResponse(Response response, Call call, Link link, Consumer<Html> consumer) {
+            try (response) {
+                // splash container restarting
+                // будет посылать запросы пока не надоест
+                if (response.code() == 503) {
+                    call.clone().enqueue(new SplashCallback(link, consumer));
+                } else if (response.code() == 504) {
+                    LoggerUtils.debugLog.error("SplashScraper - Timeout expired " + link);
+                } else if (response.code() == 200) {
+                    var responseBody = response.body();
+                    if (responseBody == null) {
+                        throw new IOException("Response body is absent");
+                    }
+                    var html = new Html(responseBody.string(), link);
+                    if (!call.isCanceled()) {
+                        consumer.accept(html);
+                    }
+                } else {
+                    throw new IOException();
+                }
+            } catch (IOException e) {
+                LoggerUtils.debugLog.error("SplashScraper - Connection failed " + link, e);
+            }
+        }
     }
 }
