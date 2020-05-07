a3b805087a0f2875123d98936c737f7bc979a111
andrey <alozhkin99@gmail.com>
2020-05-08 00:04:34 +0300

    refactor Spider

--- src/main/java/spider/Spider.java
+++ src/main/java/spider/Spider.java
@@ -27,6 +27,7 @@ public class Spider {
     private final Set<String> scrapedDomains = new HashSet<>();
 
     private int connectFailsCount = 0;
+    private Link domain;
 
     public Spider(ContextFactory contextFactory, Database database) {
         this.contextFactory = contextFactory;
@@ -42,46 +43,20 @@ public class Spider {
 
     public void scrapeDomains(Collection<Link> domains) {
         var domainExec = Executors.newSingleThreadScheduledExecutor();
-        var dbExec = Executors.newSingleThreadScheduledExecutor();
+        ScheduledExecutorService dbExec = Executors.newSingleThreadScheduledExecutor();
 
         var requestFactory = new DefaultSplashRequestFactory();
 
         try {
-            for (Link domain : domains) {
-                var fixed = domain.fixWWW().getHost();
-                if (scrapedDomains.contains(fixed)) {
-                    LoggerUtils.debugLog.info("Spider - Skip domain because is it already scraped " + domain);
-                    LoggerUtils.consoleLog.info("Skip domain because is it already scraped " + domain);
-                    continue;
-                }
-                scrapedDomains.add(fixed);
-                Statistic.reset();
+            for (Link d : domains) {
+                domain = d;
+                if (checkDomainAlreadyWas()) continue;
                 var context = contextFactory.createContext();
-                Set<String> allWords = ConcurrentHashMap.newKeySet();
                 var scraper = new SplashScraper(requestFactory);
+                Set<String> allWords = ConcurrentHashMap.newKeySet();
                 var future = domainExec.submit(() -> new DomainTask(domain, context, scraper, allWords).scrapeDomain());
-                try {
-                    future.get(DOMAIN_TIMEOUT, TimeUnit.SECONDS);
-                    connectFailsCount = 0;
-                } catch (TimeoutException e) {
-                    future.cancel(true);
-                    LoggerUtils.debugLog.error("Spider - Waiting too long for scraping site " + domain);
-                    LoggerUtils.consoleLog.error("Waiting too long for scraping site " + domain);
-                } catch (ExecutionException e) {
-                    var exClass = e.getCause().getClass();
-                    if (exClass.equals(ConnectionException.class)) {
-                        LoggerUtils.debugLog.error("DomainTask - Request failed " + domain, e);
-                        LoggerUtils.consoleLog.error("Request failed " + domain + " " + e.getMessage());
-                        ++connectFailsCount;
-                        if (connectFailsCount == CONNECT_FAILS) {
-                            throw new ConnectionException("Too many connect fails");
-                        }
-                    } else  {
-                        throw e;
-                    }
-                }
-                LoggerUtils.consoleLog.info(Statistic.string() + " site "  + domain);
-                LoggerUtils.debugLog.info(Statistic.string() + " site "  + domain);
+                handleDomainFuture(future);
+                trackStatistic();
                 dbExec.submit(new DatabaseTask(database, domain, allWords)::run);
             }
         } catch (InterruptedException e) {
@@ -98,17 +73,59 @@ public class Spider {
             LoggerUtils.debugLog.error("Spider - Failed", e);
         } finally {
             LoggerUtils.debugLog.info("Spider - Completed");
-            //todo вспомнить почему здесь есть awaitTermination, а там нет.
-            domainExec.shutdown();
-            try {
-                domainExec.awaitTermination(10, TimeUnit.SECONDS);
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                LoggerUtils.debugLog.error("Spider - Interrupted", e);
-            }
-            dbExec.shutdown();
+            shutdownExecutorService(dbExec);
+            shutdownExecutorService(domainExec);
             SplashScraper.shutdown();
             LoggerUtils.debugLog.info("Spider - Resources were closed");
+        }
+    }
+
+    private boolean checkDomainAlreadyWas() {
+        var fixed = domain.fixWWW().getHost();
+        if (scrapedDomains.contains(fixed)) {
+            LoggerUtils.debugLog.info("Spider - Skip domain because is it already scraped " + domain);
+            LoggerUtils.consoleLog.info("Skip domain because is it already scraped " + domain);
+            return true;
+        }
+        scrapedDomains.add(fixed);
+        return false;
+    }
+
+    private void handleDomainFuture(Future<?> future) throws InterruptedException, ExecutionException {
+        try {
+            future.get(DOMAIN_TIMEOUT, TimeUnit.SECONDS);
+            connectFailsCount = 0;
+        } catch (TimeoutException e) {
+            future.cancel(true);
+            LoggerUtils.debugLog.error("Spider - Waiting too long for scraping site " + domain);
+            LoggerUtils.consoleLog.error("Waiting too long for scraping site " + domain);
+        } catch (ExecutionException e) {
+            var exClass = e.getCause().getClass();
+            if (exClass.equals(ConnectionException.class)) {
+                LoggerUtils.debugLog.error("DomainTask - Request failed " + domain, e);
+                LoggerUtils.consoleLog.error("Request failed " + domain + " " + e.getMessage());
+                ++connectFailsCount;
+                if (connectFailsCount == CONNECT_FAILS) {
+                    throw new ConnectionException("Too many connect fails");
+                }
+            } else  {
+                throw e;
+            }
+        }
+    }
+
+    private void trackStatistic() {
+        LoggerUtils.consoleLog.info(Statistic.string() + " site "  + domain);
+        LoggerUtils.debugLog.info(Statistic.string() + " site "  + domain);
+        Statistic.reset();
+    }
+
+    private void shutdownExecutorService(ExecutorService executorService) {
+        executorService.shutdown();
+        try {
+            executorService.awaitTermination(10, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
         }
     }
 }
